b300c39f6867c8ab28805395b29b12d8be52e441
shiro <shiro.mns@gmail.com>
2018-09-14 03:04:10 +0900

    [追加]モデルの関連付け、属性防御の列に色付け

--- app/models/next_battle.rb
+++ app/models/next_battle.rb
@@ -1,3 +1,7 @@
 class NextBattle < ApplicationRecord
-	belongs_to :p_name,	    :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Name'
+	belongs_to :p_name,	            :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Name'
+	belongs_to :status,	            :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Status'
+	belongs_to :spec,	            :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Spec'
+	belongs_to :reward,	            :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Reward'
+	belongs_to :condition_all_text,	:foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'ConditionAllText'
 end
