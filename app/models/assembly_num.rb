2955f1438514d5e2eb478f6428fe7de6b6369fea
shiro <shiro.mns@gmail.com>
2018-09-16 15:15:08 +0900

    [修正]アセンブルの装備番号範囲指定のミス

--- app/models/assembly_num.rb
+++ app/models/assembly_num.rb
@@ -5,5 +5,5 @@ class AssemblyNum < ApplicationRecord
 	belongs_to :next_battle,:foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'NextBattle'
 	belongs_to :parts_name, :foreign_key => :proper_name_id, :primary_key => :proper_id, :class_name => 'ProperName'
 	belongs_to :leg,      -> { where(equip: 3)},     :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Item'
-	has_many   :assembly, -> { where(equip: 1..10)}, :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Item'
+	has_many   :assembly, -> { where(equip: 1..12)}, :foreign_key => [:e_no, :result_no, :generate_no], :primary_key => [:e_no, :result_no, :generate_no], :class_name => 'Item'
 end
