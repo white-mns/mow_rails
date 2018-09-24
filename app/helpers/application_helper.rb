73089cd37d787a0b9e86570d5669316c83b0fc2f
shiro <shiro.mns@gmail.com>
2018-09-25 05:33:07 +0900

    [追加]キャラ名表示の共通関数化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,15 @@ module ApplicationHelper
         title
     end
 
+    def p_name_text(e_no, p_name)
+        e_no_text = "(" + sprintf("%d",e_no) + ")"
+        if p_name then
+            p_name.name.html_safe + e_no_text
+        else
+            e_no_text
+        end
+    end
+
     def character_link(e_no)
         file_name = sprintf("%04d",e_no)
         link_to " 結果", "http://blacktea.sakura.ne.jp/mistofwar/RESULT/c"+file_name+".html", :target => "_blank"
