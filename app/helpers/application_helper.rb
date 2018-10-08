1f4a4c1ec4b7a9819825469898fe2a7ed2b1e2ba
shiro <shiro.mns@gmail.com>
2018-10-08 10:03:19 +0900

    [変更]Enoが0以下の場合結果リンクなし。返り値なしのときに先にreturnするように。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,17 +15,20 @@ module ApplicationHelper
     end
 
     def character_link(e_no)
+        if e_no <= 0 then return end
+
         file_name = sprintf("%04d",e_no)
         link_to " 結果", "http://blacktea.sakura.ne.jp/mistofwar/RESULT/c"+file_name+".html", :target => "_blank"
     end
     
     def character_old_link(last_result_no, e_no, result_no, generate_no)
-        if result_no < last_result_no
-            file_name = sprintf("%04d", e_no)
-            result_no_text = sprintf("%03d", result_no)
-            generate_text  = generate_no > 0 ? "_" + sprintf("%d", generate_no) : ""
-            link_to " 過去結果", "http://mistofwar.kitunebi.com/M_o_W_5/"+result_no_text+generate_text+"/RESULT/c"+file_name+".html", :target => "_blank"
-        end
+        if e_no <= 0 then return end
+        if result_no ==  last_result_no then return end
+
+        file_name = sprintf("%04d", e_no)
+        result_no_text = sprintf("%03d", result_no)
+        generate_text  = generate_no > 0 ? "_" + sprintf("%d", generate_no) : ""
+        link_to " 過去結果", "http://mistofwar.kitunebi.com/M_o_W_5/"+result_no_text+generate_text+"/RESULT/c"+file_name+".html", :target => "_blank"
     end
 
     def all_assembly_text(assembly)
